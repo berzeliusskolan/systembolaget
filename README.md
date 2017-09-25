d914f9828ff4994191aabe6a9bfa97bf1371efbe
Johan Kellen <johan.kellen@linkoping.se>
2017-09-25 05:59:00 +0000

    Start

---
+++ README.md
@@ -0,0 +1,5 @@
+# Uppgift Systembolaget
+Du ska skriva ut en tabell med Systembolaget Umeås sortiment. 
+Tabellen ska skapas utifrån systembolaget.json. Denna fil finns bifogad.
+Tabellen ska innehålla svenska tecken. 
+Tabellen ska innehålla ikoner för kategori. Ikoner för dessa finns bifogade.
---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+//ta in våra vendor-lib, ex Kint
+require_once "../vendor/autoload.php";
+
+//läsa fildata
+// $jsonData = file_get_contents("yourfile.json");
+
+//koda om json-sträng till array
+// $arr = json_decode($yourJsonData, true);
+
+//din logik för att skriva ut en snygg tabell
+
+
+//använd gärna Bootstrap för att det ska se snyggare ut
+//glöm inte att ha en div med klassen container runt alltihop
+//<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+//<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
